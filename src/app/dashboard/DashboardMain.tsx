1a0a64ee6cdb4de86a2c8e6b34221589ad6d27af
kalynbeach <kalynbeach@gmail.com>
2023-04-12 20:38:46 -0700

    add DashboardMain and IndexList, clean up pages

---
+++ src/app/dashboard/DashboardMain.tsx
@@ -0,0 +1,7 @@
+export default function DashboardMain() {
+  return (
+    <div className='w-full p-4 flex flex-col justify-between border rounded border-neutral-900'>
+      <span>Dashboard Main</span>
+    </div>
+  )
+}
---
+++ src/components/IndexList.tsx
@@ -0,0 +1,7 @@
+export default function IndexList() {
+  return (
+    <div className='w-full p-4 flex flex-col justify-between border rounded border-neutral-900'>
+      <span>Index List</span>
+    </div>
+  )
+}
