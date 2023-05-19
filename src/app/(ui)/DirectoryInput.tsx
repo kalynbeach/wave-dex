d1b4bee03cffbb5a829aa2be599fe956c71100d5
kalynbeach <kalynbeach@gmail.com>
2023-05-18 19:27:33 -0700

    add basic FileList filtering in DirectoryInput

--- src/app/(ui)/DirectoryInput.tsx
+++ src/app/(ui)/DirectoryInput.tsx
@@ -2,12 +2,29 @@
 
 export default function DirectoryInput() {
 
+  function filterFiles(files: FileList, fileTypes: string[]): File[] {
+    const fileCount = files.length
+    let filteredFiles: File[] = []
+    for (let i = 0; i < fileCount; i++) {
+      let file = files[i]
+      if (fileTypes.includes(file.type)) {
+        filteredFiles.push(file)
+      }
+    }
+    return filteredFiles
+  }
+
   function handleChange(event: React.ChangeEvent<HTMLInputElement>) {
     const files = event.target.files
+    let filteredFiles: File[] = []
+
     if (files) {
-      console.log(`[DirectoryInput] Files: `, files)
-      // TODO: Handle files
+      console.log(`[DirectoryInput] File count: `, files.length)
+      const fileTypes = ['audio/x-wav']
+      filteredFiles = filterFiles(files, fileTypes)
     }
+
+    console.log(`[DirectoryInput] Filtered Files: `, filteredFiles)
   }
 
   return (
